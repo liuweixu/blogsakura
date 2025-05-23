9b6b85989c16d7026a94224baca544a5752994df
liuweixu <liuweixu2018@gmail.com>
2025-05-23 14:01:41 +0800

    初步分开前台后台Router，让<header>只影响前台，但是技术不成熟

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
 import "./index.css";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 //在别的文件夹合并store和router
 import { store } from "./storeall";
-import { router } from "./routerall";
+
 import { Toaster } from "sonner";
 import "@ant-design/v5-patch-for-react-19";
-// import { Header } from "@/ui-frontend/pages/header";
+import { Header } from "./ui-frontend/pages/header";
+import { RouterBackend } from "@/ui-backend/router";
+import { RouterFrontend } from "./ui-frontend/router";
+import { BrowserRouter } from "react-router-dom";
 
+const location = window.location;
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    {/* <BrowserRouter> */}
-    {/* <Header /> */}
-    <RouterProvider router={router}></RouterProvider>
-    <Toaster />
-
-    {/* </BrowserRouter> */}
+    <BrowserRouter>
+      {!location.pathname.startsWith("/backend/") && <Header />}
+      <RouterFrontend />
+      <RouterBackend />
+      <Toaster />
+    </BrowserRouter>
   </Provider>
 );
--- src/ui-frontend/router/index.tsx
+++ src/ui-frontend/router/index.tsx
@@ -1,21 +1,14 @@
-import { createBrowserRouter, type RouteObject } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { HomeTest } from "@/ui-frontend/pages/hometest";
 import { Home } from "@/ui-frontend/pages/home";
 import { Article } from "@/ui-frontend/pages/article";
 
-export const routes: RouteObject[] = [
-  {
-    path: "/test",
-    element: <HomeTest />,
-  },
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/article/:id", // 注意这里添加了斜杠和冒号
-    element: <Article />,
-  },
-];
-
-export const router = createBrowserRouter(routes);
+export const RouterFrontend = () => {
+  return (
+    <Routes>
+      <Route path="/test" element={<HomeTest />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/article/:id" element={<Article />} />
+    </Routes>
+  );
+};
