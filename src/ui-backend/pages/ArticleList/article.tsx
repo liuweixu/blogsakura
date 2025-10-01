0dd62bccb23d9c119d8f8cb441faea1bf256e15e
liu <liu@liudeMacBook-Air.local>
2025-10-01 09:44:35 +0800

    初步完成文章列表静态结构

--- src/ui-backend/pages/ArticleList/article.tsx
+++ src/ui-backend/pages/ArticleList/article.tsx
@@ -1,153 +1,145 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-
-import { getArticleListAPI } from "@/ui-backend/apis/article";
+import { Link, useNavigate } from "react-router-dom";
+import { Breadcrumb, Card, Form, Radio, Select, Button, DatePicker, Popconfirm } from 'antd';
 import { useEffect, useState } from "react";
-import type { ArticleItem } from "@/ui-backend/interface/Article";
+import { getChannelAPI } from "@/ui-backend/apis/article";
+import type { ChannelItem } from "@/ui-backend/interface/Publish";
+// 引入汉化包 时间选择器显示中文
+import locale from 'antd/es/date-picker/locale/zh_CN'
 
-import { FcFullTrash } from "react-icons/fc";
-import { FcEditImage } from "react-icons/fc";
+import { Table, Tag, Space } from 'antd'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
-import { delArticleAPI } from "@/ui-backend/apis/article";
-import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
-import { Card } from "@/components/ui/card";
 
-import { usePagination } from "@/ui-backend/components/Pagination";
-import { Header } from "@/ui-backend/components/Header";
+const { Option } = Select;
+const { RangePicker } = DatePicker;
 
-export function Article() {
-  //必须要有接口，否则下面的invoices进行map时候，很容易出现错误提示
-  const [invoices, setInvoices] = useState<ArticleItem[]>([]);
-  //导航
-  const navigator = useNavigate();
-  //计算得到文章列表的总数
-  const [count, setCount] = useState(0);
-  //使用分页组件
-  const { pagination, itemPerPage, currentPage } = usePagination(count);
-  //渲染页面的时候，获取文章列表
+export const Article = () => {
+  //获取频道信息
+  const [channelList, setChannelList] = useState<ChannelItem[]>([]);
+  const navigate = useNavigate();
   useEffect(() => {
-    const getArticleList = async () => {
-      const res = await getArticleListAPI();
-      setInvoices(res.data);
-      setCount(res.data.length);
+    const getChannelList = async () => {
+      const res = await getChannelAPI();
+      setChannelList(res.data);
     };
-    getArticleList();
+    getChannelList();
   }, []);
 
-  //计算当前页显示的文章
-  const currentItems = invoices.slice(
-    (currentPage - 1) * itemPerPage,
-    currentPage * itemPerPage
-  );
-
-  //删除文章事件
-  const handlerDeleteArticle = async (id: string) => {
-    console.log(id);
-    await delArticleAPI(id);
-    // 很关键，涉及到界面的再次渲染
-    const res = await getArticleListAPI();
-    setInvoices(res.data);
-    setCount(res.data.length);
-  };
-
-  //对AlertDialog组件的封装
-  const renderAlertDialog = (id: string) => {
-    return (
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <FcFullTrash>
-            <Button>删除</Button>
-          </FcFullTrash>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>确认删除？</AlertDialogTitle>
-            <AlertDialogDescription>
-              此操作不可逆，请谨慎操作。
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>取消</AlertDialogCancel>
-            <AlertDialogAction onClick={() => handlerDeleteArticle(id)}>
-              确认
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    );
-  };
-
+  // 列表头
+  // 准备列数据
+  // 定义状态枚举
+  const columns = [
+    {
+      title: '封面',
+      dataIndex: 'cover',
+      width: 120,
+      render: cover => {
+        return <img src={cover.images[0] || img404} width={80} height={60} alt="" />
+      }
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      width: 220
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      // data - 后端返回的状态status 根据它做条件渲染
+      // data === 1 => 待审核
+      // data === 2 => 审核通过
+      render: data => status[data]
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'pubdate'
+    },
+    {
+      title: '阅读数',
+      dataIndex: 'read_count'
+    },
+    {
+      title: '评论数',
+      dataIndex: 'comment_count'
+    },
+    {
+      title: '点赞数',
+      dataIndex: 'like_count'
+    },
+    {
+      title: '操作',
+      render: data => {
+        return (
+          <Space size="middle">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} onClick={() => navigate(`/publish?id=${data.id}`)} />
+            <Popconfirm
+              title="删除文章"
+              description="确认要删除当前文章吗?"
+              // onConfirm={() => onConfirm(data)}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button
+                type="primary"
+                danger
+                shape="circle"
+                icon={<DeleteOutlined />}
+              />
+            </Popconfirm>
+          </Space>
+        )
+      }
+    }
+  ]
   return (
     <div>
-      <Header href="/backend/home" title="首页" pagename="文章列表"></Header>
-      <div className="mb-5 mt-5">{`文章总数：${count}`}</div>
-      <Card>
-        <Table className="min-h-[300px] text-lg">
-          <TableCaption>文章列表</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[200px]">标题</TableHead>
-              <TableHead className="w-[500px]">频道</TableHead>
-              <TableHead className="w-[600px]">频道ID</TableHead>
-              <TableHead>
-                <div className="flex justify-center">操作</div>
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {/**TODO 关键 ，能够实现分页机制的关键：让currentItems替代invoices */}
-            {currentItems.map((invoice) => (
-              <TableRow key={invoice.id}>
-                <TableCell className="font-medium">{invoice.title}</TableCell>
-                <TableCell>{invoice.channel_name}</TableCell>
-                <TableCell>{invoice.channel_id}</TableCell>
-                <TableCell>
-                  {
-                    <div className="flex justify-center">
-                      <FcEditImage
-                        className="mr-5"
-                        onClick={() => {
-                          navigator(`/backend/publish?id=${invoice.id}`);
-                        }}
-                      />
-                      {renderAlertDialog(invoice.id)}
-                    </div>
-                  }
-                </TableCell>
-              </TableRow>
+      <Breadcrumb
+        separator='>'
+        items={[
+          {
+            title: '首页',
+            href: '/backend/home',
+          },
+          {
+            title: '文章列表',
+            href: '/backend/articlelist',
+          },
+        ]} />
+      <Card style={{ marginTop: 20 }}>
+        <Form initialValues={{ status: null }}>
+          <Form.Item label='状态' name='status'>
+            <Radio.Group>
+              <Radio value={null}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={2}>审核通过</Radio>
+            </Radio.Group>
+          </Form.Item>
+        </Form>
+        <Form.Item label='频道' name='channel_id'>
+          <Select placeholder='请选择文章频道' style={{ width: 120 }}>
+            {channelList.map((channel) => (
+              <Option key={channel.id} value={channel.name}>
+                {channel.name}
+              </Option>
             ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              {/**关键操作，让分页居中 使用 colSpan={3} 让分页组件横跨所有列 添加 className="text-center" 使内容居中*/}
-              <TableCell colSpan={4} className="text-center">
-                {pagination}
-              </TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
+          </Select>
+        </Form.Item>
+        <Form.Item label='日期' name='date'>
+          <RangePicker locale={locale}></RangePicker>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ marginLeft: 40 }}>
+            筛选
+          </Button>
+        </Form.Item>
+      </Card>
+      {/**表格区域 */}
+      <Card style={{ marginTop: 20 }}>
+        <Table rowKey="id" columns={columns} />
       </Card>
     </div>
-  );
+  )
 }
+
+
+
